516263e46c4cca00e0e787f54ea776afb4e9b49f
Yxin <xiaoming@YXIN.local>
2021-03-17 08:59:16 +0800

    Updated comments in ServerSelector. Implementing.

--- SDK_C++/CodeCraft-2021/server_selector.cpp
+++ SDK_C++/CodeCraft-2021/server_selector.cpp
@@ -8,10 +8,10 @@ ServerSelector::ServerSelector() :
 }
 
 // 1. Sort the servers according to some criteria
-// 2. Calculate the upper bound of the cost
-// 3. Add the top server to the list
-// 4. Add vm's until it can fit no more (knapsack where value of each vm = size of each vm)
-// 5. Repeat 2-4 until all tasks have been added
+// 2. Calculate the upper bound of the cost when using only the top server (muiti knapsack where value of each vm = size of each vm)
+// 3. Remove one server from the back of the list
+// 4. Repeat 2-3 with the next to the top server, by adding the next to the top server to the back of the list.
+// 5. Repeat 2-4 until all server combo have been tried
 void ServerSelector::MakeServerSelection() {
     
 }
