dcf4f162aee377b41d0af26b9893805897a068b8
Yxin <xiaoming@YXIN.local>
2021-03-18 22:31:55 +0800

    Added way to arrange worst case vm num in vm_manager. Adding count for each vm.

--- SDK_C++/CodeCraft-2021/vm_manager.cpp
+++ SDK_C++/CodeCraft-2021/vm_manager.cpp
@@ -1,6 +1,7 @@
 // @Author: Yu Xin
 
 #include <iostream>
+#include <unordered_set>
 
 #include "vm_manager.h"
 
@@ -11,6 +12,7 @@ VmManager::VmManager() :
   num_vm_(vm_data_manager_.GetNumVm()),
   request_info_list_(request_data_manager_.GetRequestInfoList()) {
     vm_schedules_.reserve(num_vm_);
+    std::cout << "initializing vm manager" << std::endl;
     for (const auto& request : request_info_list_) {
         const int32_t request_id = request.first;
         const RequestInfo& request_info = request.second;
@@ -19,10 +21,36 @@ VmManager::VmManager() :
         // std::cout << "Request " << request_id << " needs vm " << vm_id << " from day " << request_info.start_day << " to day " << request_info.end_day << std::endl;
 
         VmStatusInfo& curr_vm_status = vm_schedules_[vm_id];
+        VmStatusWorstCaseInfo& curr_vm_status_worst_case = vm_schedules_worst_case_[vm_id];
         curr_vm_status.num_running.resize(days_);
+
+        // For finding the lowest ranked vm that can run this request
+        std::unordered_set<int> possible_vms;
+        for (int i = 0; i < curr_vm_status_worst_case.vm_schedule_list.size(); ++i) {
+            possible_vms.insert(i);
+        }
+        for (int16_t day = request_info.start_day; day <= request_info.end_day && !possible_vms.empty(); ++day) {
+            for (auto it = possible_vms.begin(); it != possible_vms.end(); ) {
+                if (curr_vm_status_worst_case.vm_schedule_list[*it][day]) {
+                    it = possible_vms.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+        int vm_scheduled_for_worst_case;
+        if (possible_vms.empty()) {
+            curr_vm_status_worst_case.vm_schedule_list.push_back(std::vector<bool>(days_, false));
+            vm_scheduled_for_worst_case = curr_vm_status_worst_case.vm_schedule_list.size() - 1;
+        } else {
+            vm_scheduled_for_worst_case = *(possible_vms.begin());
+        }
+        std::cout << "vm_scheduled_for_worst_case is " << vm_scheduled_for_worst_case << std::endl;
+
         for (int16_t day = request_info.start_day; day <= request_info.end_day; ++day) {
             ++curr_vm_status.num_running[day];
             curr_vm_status.request_id_list[day].push_back(request_id);
+            curr_vm_status_worst_case.vm_schedule_list[vm_scheduled_for_worst_case][day] = true;
             // std::cout << "Request " << curr_vm_status.request_id_list[day].back()
             //     << " added, we have " << curr_vm_status.num_running[day] << " "
             //     << request_info.requested_vm_name << " running on day " << day << std::endl;
--- SDK_C++/CodeCraft-2021/vm_manager.h
+++ SDK_C++/CodeCraft-2021/vm_manager.h
@@ -25,6 +25,15 @@ struct VmStatusInfo {
     std::unordered_map<int32_t, std::vector<int32_t> > server_id_list;
 };
 
+// Status of this type of vm for calc worst case, including how many of this type of vm is running on each day
+// and the schedules of these vm's
+struct VmStatusWorstCaseInfo {
+    // std::vector<uint32_t> num_running;
+    std::vector<std::vector<bool> > vm_schedule_list; // schedule of each vm
+
+    // Day, list of server id; currently unused, but might need it later when mapping VM's to servers
+    // std::unordered_map<int32_t, std::vector<int32_t> > server_id_list;
+};
 
 class VmManager {
   public:
@@ -39,10 +48,11 @@ class VmManager {
 
     VmDataManager& vm_data_manager_;
     RequestDataManager& request_data_manager_;
-    uint16_t days_;
+    uint16_t days_; // Number of days (T)
     uint16_t num_vm_;
     std::unordered_map<int32_t, RequestInfo>& request_info_list_;
     // vm id to a list of schedules of all vm's of this kind
     // TODO(Yu Xin): use next_available when adding requests to the list
     std::unordered_map<uint16_t, VmStatusInfo> vm_schedules_;
+    std::unordered_map<uint16_t, VmStatusWorstCaseInfo> vm_schedules_worst_case_;
 };
