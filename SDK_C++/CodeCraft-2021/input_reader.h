d9a9bf8f5ed3f1896a9fa20f0e4dcfff57d7601f
Yxin <xiaoming@ipsec-172-16-78-151.vpn.gatech.edu>
2021-03-20 12:40:34 +0800

    Added various indices

--- SDK_C++/CodeCraft-2021/input_reader.h
+++ SDK_C++/CodeCraft-2021/input_reader.h
@@ -20,6 +20,7 @@ struct ServerInfo {
     int16_t server_memory; // range [1, 1024] positive number
     int16_t running_cost; // range [1, 5000]  positive number
     int32_t purchase_cost; // range [1, 5*1e5] positive number
+    float server_lambda; // lambda is CPU / Memory
     std::string server_name; // string length is at most 20 characters (number&alphabet)
 };
 
--- SDK_C++/CodeCraft-2021/vm_data_manager.h
+++ SDK_C++/CodeCraft-2021/vm_data_manager.h
@@ -19,16 +19,25 @@ class VmDataManager {
     VmInfo& GetVm(const std::string& vm_name);
     int GetVmId(const std::string& vm_name);
     VmInfo& GetVmNthLambda(const uint16_t n); // Lambda is CPU / Memory
+    uint16_t GetNumSingleVm(const uint16_t n);
+    uint16_t GetNumDoubleVm(const uint16_t n);
+    VmInfo& GetNthSingleVm(const uint16_t n);
+    VmInfo& GetNthDoubleVm(const uint16_t n);
 
   private:
     VmDataManager();
     ~VmDataManager() = default;
     InputReader& input_reader_;
     uint16_t num_vm_;
+    uint16_t num_single_vm_;
+    uint16_t num_double_vm_;
     std::vector<VmInfo>& vm_info_list_;
-    std::unordered_map<std::string, int> index_vm_name_;
-    std::vector<int> index_vm_lambda_;
+    std::unordered_map<std::string, uint16_t> index_vm_name_;
+    std::vector<uint16_t> index_vm_lambda_;
+    std::vector<uint16_t> index_single_vm_;
+    std::vector<uint16_t> index_double_vm_;
 
     void BuildIndexVmLambda();
+    void BuildIndexVmSinDou();
     
 };
