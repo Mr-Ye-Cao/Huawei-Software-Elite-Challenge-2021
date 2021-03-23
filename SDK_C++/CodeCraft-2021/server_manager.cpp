acd4f46bee0d5ff64653dafc9f63e67a08a448ee
Yxin <xiaoming@ipsec-172-16-79-140.vpn.gatech.edu>
2021-03-23 22:00:59 +0800

    Added server manager back

---
+++ SDK_C++/CodeCraft-2021/server_manager.cpp
@@ -0,0 +1,5 @@
+// @Author: Yu Xin
+
+#include "server_manager.h"
+
+
--- SDK_C++/CodeCraft-2021/server_manager.h
+++ SDK_C++/CodeCraft-2021/server_manager.h
@@ -1,35 +1,35 @@
-// // @Author: Ye Cao  Zhongyan Wang
+// @Author: Ye Cao  Zhongyan Wang
 
-// #pragma once
+#pragma once
 
-// #include <vector>
+#include <vector>
 
-// #include "request_data_manager.h"
-// #include "server_data_manager.h"
+#include "request_data_manager.h"
+#include "server_data_manager.h"
 
-// struct Server {
-//     // (TODO) a vector containing the Vm tasks 
+struct Server {
+    // (TODO) a vector containing the Vm tasks 
 
-//     int16_t server_cpu_A; // The cpu number at node A
-//     int16_t server_cpu_B; // The cpu number at node B
+    int16_t server_cpu_A; // The cpu number at node A
+    int16_t server_cpu_B; // The cpu number at node B
 
-//     int16_t server_mem_A; // The memory number at node A
-//     int16_t server_mem_B; // The memory number at node A
+    int16_t server_mem_A; // The memory number at node A
+    int16_t server_mem_B; // The memory number at node A
 
-//     int16_t id; // Dynamic Id
-// };
+    int16_t id; // Dynamic Id
+};
 
-// class ServerManager {
-//   public:
-//     // Meyer singleton
-//     static ServerManager& GetInstance();
-//     ServerManager(const ServerManager&) = delete;
-//     ServerManager& operator=(const ServerManager&) = delete;
+class ServerManager {
+  public:
+    // Meyer singleton
+    static ServerManager& GetInstance();
+    ServerManager(const ServerManager&) = delete;
+    ServerManager& operator=(const ServerManager&) = delete;
     
-//     std::vector<Server> server_cluster; 
+    std::vector<Server> server_cluster; 
 
-//   private:
-//     ServerManager();
-//     ~ServerManager() = default;
+  private:
+    ServerManager();
+    ~ServerManager() = default;
 
-// };
+};
